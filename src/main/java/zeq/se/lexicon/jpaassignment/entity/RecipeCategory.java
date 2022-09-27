92a48bd41985dc10bdd076354e0b0b579a64cf50
Zeq Alidemaj <zeq.alidemaj@gmail.com>
2022-09-27 12:43:20 +0200

    * Added Repository

--- src/main/java/zeq/se/lexicon/jpaassignment/entity/RecipeCategory.java
+++ src/main/java/zeq/se/lexicon/jpaassignment/entity/RecipeCategory.java
@@ -6,38 +6,36 @@ import java.util.Objects;
 import java.util.Set;
 
 @Entity
+
 //@Table(name = "Recipecategory")
 public class RecipeCategory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-@Column(name = "recipe_category_id")
+    @Column(name = "recipe_category_id")
     private Integer id;
     //@Column(name = "category")
     private String category;
     @ManyToMany(mappedBy = "categories", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
-    private Set<Recipe> recipe = new HashSet<>();
+    private Set<Recipe> recipes = new HashSet<>();
 
     public RecipeCategory() {
     }
 
-    public RecipeCategory(int id, String category, Set<Recipe> recipe) {
-        this.id = id;
+    public RecipeCategory(String category, Set<Recipe> recipes) {
         this.category = category;
-        this.recipe = recipe;
+        this.recipes = recipes;
     }
 
-    public RecipeCategory(String category, Set<Recipe> recipe) {
-        this.category = category;
-        this.recipe = recipe;
+    public RecipeCategory(String vegetarian, String recipies) {
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -49,25 +47,54 @@ public class RecipeCategory {
         this.category = category;
     }
 
-    public Set<Recipe> getRecipe() {
-        return recipe;
+    public Set<Recipe> getRecipes() {
+        return recipes;
     }
 
-    public void setRecipe(Set<Recipe> recipe) {
-        this.recipe = recipe;
+    // toDO ******************************************
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
     }
+
+    //toDO *******************************************
+    public void addRecipe(Recipe recipe){
+
+        if(recipe == null) throw new IllegalArgumentException("This recipe is null");
+
+        /*********** Add ************************/
+        if(recipes == null) recipes = new HashSet<>();
+
+        if(!recipes.contains(recipe))
+            recipe.getCategories().add(this);
+            recipes.add(recipe);
+
+    }
+
+    /**** Remove ***/
+    public void removeRecipe(Recipe recipe) {
+
+        if (recipe == null) throw new IllegalArgumentException("Recipe already in exists in db");
+        if (recipes == null) setRecipes(new HashSet<>());
+
+        if(recipes.contains(recipe)) {
+            recipe.getCategories().remove(this);
+            recipes.remove(recipe);
+        }
+    }
+
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         RecipeCategory that = (RecipeCategory) o;
-        return Objects.equals(getCategory(), that.getCategory()) && Objects.equals(getRecipe(), that.getRecipe());
+        return Objects.equals(getCategory(), that.getCategory()) && Objects.equals(getRecipes(), that.getRecipes());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getCategory(), getRecipe());
+        return Objects.hash(getCategory(), getRecipes());
     }
 
     @Override
@@ -75,7 +102,8 @@ public class RecipeCategory {
         return "RecipeCategory{" +
                 "id=" + id +
                 ", category='" + category + '\'' +
-                ", recipe=" + recipe +
+                ", recipes=" + recipes +
                 '}';
     }
 }
+
